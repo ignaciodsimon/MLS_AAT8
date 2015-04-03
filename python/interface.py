acdca108158d687b687bc96ce2ef3783761e93eb
jdomin14 <jdomin14@student.aau.dk>
2015-04-03 18:52:34 +0200

    Basic skeleton with the business logic. Functional, no user interface. Estimates IR from two channels and de-convolves the two IR into one.

--- python/interface.py
+++ python/interface.py
@@ -12,14 +12,60 @@ Parts:
 Joe.
 """
 
-import recorder
-import numpy
 import matplotlib.pyplot as plot
 
-data = recorder.rec(numOfChannels=2, recordingLength=60000, samplFreq=44100, bitDepth=16)
+# Internal imports
+import recorder
+import parallelfunctions
+import player
+import computeIR
+import generateMLS
 
 
-# print data
-print numpy.shape(data)
-# plot.plot(data)
-# plot.show()
+# ----------= TEST CONFIGURATION =----------
+_MLSLength = 32768
+_channels = 2
+_samplFreq = 44100
+_signalAmplitude = 0.5
+_preDelayForPlayback = 0.25
+_decayTime = 5.0
+
+
+# ----------= TEST EXECUTION =----------
+# Generate the MLS signal
+print "Generating MLS signal ..."
+_MLSSignal = generateMLS.generateMLS(_MLSLength, _signalAmplitude)
+
+# Padding MLS signal with zeros at the beginning
+_MLS_WithZeros = [0] * int(round(_preDelayForPlayback * _samplFreq) + _MLSLength)
+_MLS_WithZeros[len(_MLS_WithZeros)-len(_MLSSignal):len(_MLS_WithZeros)] = _MLSSignal
+_MLSSignal = _MLS_WithZeros
+
+# Recording length is function of MLS length and the system decay time
+_recordingLength = len(_MLSSignal) + int(_samplFreq * _decayTime)
+
+# Player and recorder input arguments in vectors for multitasking
+_recorderArguments = [_channels, _recordingLength, _samplFreq, 16]
+_playerArguments = [_MLSSignal, _MLSSignal, _samplFreq, False]
+
+# Runs player and recorder simultaneously
+print "Running multitask ..."
+[_recorderOutputData, _playerOutputData] = parallelfunctions.runInParallel(recorder.rec, _recorderArguments, player.playSignals, _playerArguments)
+
+# Computes IR
+print "Computing IRs ..."
+channelL = _recorderOutputData[0][:]
+channelR = _recorderOutputData[1][:]
+
+# Padding with zeros of MLS signal
+_paddedMLSSignal = [0]*(len(channelL))
+_paddedMLSSignal[0:len(_MLSSignal)] = _MLSSignal
+
+_IRLeft = computeIR.computeCircularXCorr(channelL, _paddedMLSSignal)
+_IRRight = computeIR.computeCircularXCorr(channelR, _paddedMLSSignal)
+_corrected = computeIR.correctSignalWithIR(_IRLeft, _IRRight)
+
+
+# ----------= DISPLAY OF RESULTS =----------
+plot.plot(_corrected)
+plot.show()
