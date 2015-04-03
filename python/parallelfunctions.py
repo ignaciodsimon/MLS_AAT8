6b02df4c86ee33e6b63a71fda30f9128030f0889
jdomin14 <jdomin14@student.aau.dk>
2015-04-03 18:50:10 +0200

    Minor corrections and style.

--- python/parallelfunctions.py
+++ python/parallelfunctions.py
@@ -1,67 +1,4 @@
-# import numpy as np
-# import datetime
 import multiprocessing as mp
-
-
-# def dummyFunction1(n, p, o, t):
-#     """
-#     This is a dummy function used to test multiprocessing.
-#     It displays the start second and end second of the function
-#
-#     """
-#
-#     now1 = datetime.datetime.now()
-#     milisec1 = now1.microsecond/1000.0
-#     print '_dummy1 started at %d seconds' % now1.second, '%f miliseconds' % milisec1
-#     A = np.ones([n, n])
-#     B = np.ones([n, n])
-#     C = A*B
-#     now2 = datetime.datetime.now()
-#     milisec2 = now2.microsecond/1000.0
-#     print '_dummy1 finished at %d seconds' % now2.second, '%f miliseconds' % milisec2
-#     print p, o, t
-#
-#     return 16
-#
-# def dummyFunction2(n, p, o):
-#     """
-#     This is a dummy function used to test multiprocessing.
-#     It displays the start time of the function
-#
-#     """
-#
-#     now3 = datetime.datetime.now()
-#     milisec3 = now3.microsecond/1000.0
-#     print '_dummy2 started at %d seconds and' % now3.second, '%f miliseconds' % milisec3
-#     A = np.ones([n, n])
-#     B = np.ones([n, n])
-#     C = A*B
-#     now4 = datetime.datetime.now()
-#     milisec4 = now4.microsecond/1000.0
-#     print '_dummy2 finished at %d seconds and' % now4.second, '%f miliseconds' % milisec4
-#     print p, o
-#
-#     return 28
-
-
-# def _multiprocessing(function1, f1arg1, f1arg2, f1arg3, f1arg4, function2, f2arg1, f2arg2, f2arg3, f2arg4):
-#     """
-#     This function takes 2 functions as inputs and runs them in parallel
-#
-#     """
-#
-#     print 'Started parallel processing'
-#
-#     pool = mp.Pool()
-#     # evaluate "function1(f1arg1, f1arg2, f1arg3, f1arg4)" asynchronously
-#     result1 = pool.apply_async(function1, [f1arg1, f1arg2, f1arg3, f1arg4])
-
-#     # evaluate "function2(f2arg1, f2arg2, f2arg3, f2arg4)" asynchronously
-#     result2 = pool.apply_async(function2, [f2arg1, f2arg2, f2arg3, f2arg4])
-#     answer1 = result1.get()
-#     answer2 = result2.get()
-#
-#     print 'Finished parallel processing'
 
 
 def runInParallel(function1, f1args, function2, f2args):
@@ -103,6 +40,50 @@ def runInParallel(function1, f1args, function2, f2args):
 
 
 # # --= Code to test multitasking ==--
+#
+# import numpy as np
+# import datetime
+#
+# def dummyFunction1(n, p, o, t):
+#     """
+#     This is a dummy function used to test multiprocessing.
+#     It displays the start second and end second of the function
+#
+#     """
+#
+#     now1 = datetime.datetime.now()
+#     milisec1 = now1.microsecond/1000.0
+#     print '_dummy1 started at %d seconds' % now1.second, '%f miliseconds' % milisec1
+#     A = np.ones([n, n])
+#     B = np.ones([n, n])
+#     C = A*B
+#     now2 = datetime.datetime.now()
+#     milisec2 = now2.microsecond/1000.0
+#     print '_dummy1 finished at %d seconds' % now2.second, '%f miliseconds' % milisec2
+#     print p, o, t
+#
+#     return 16
+#
+# def dummyFunction2(n, p, o):
+#     """
+#     This is a dummy function used to test multiprocessing.
+#     It displays the start time of the function
+#
+#     """
+#
+#     now3 = datetime.datetime.now()
+#     milisec3 = now3.microsecond/1000.0
+#     print '_dummy2 started at %d seconds and' % now3.second, '%f miliseconds' % milisec3
+#     A = np.ones([n, n])
+#     B = np.ones([n, n])
+#     C = A*B
+#     now4 = datetime.datetime.now()
+#     milisec4 = now4.microsecond/1000.0
+#     print '_dummy2 finished at %d seconds and' % now4.second, '%f miliseconds' % milisec4
+#     print p, o
+#
+#     return 28
+#
 #
 # _dummy1args = [100, 1, 1, 1]
 # _dummy2args = [1000, 2, 2]
