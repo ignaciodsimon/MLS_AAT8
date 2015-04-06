ebfa99065508a4f3fcdafd89ab449a8e0fa7f6e1
jdomin14 <jdomin14@student.aau.dk>
2015-04-06 10:34:30 +0200

    Created strings file for language strings.

---
+++ python/strings.py
@@ -0,0 +1,39 @@
+"""
+Language strings.
+"""
+
+
+TEXT_1 = "  1 - Hardware settings  "
+TEXT_2 = "Input device:"
+TEXT_3 = "Output device:"
+TEXT_4 = "- No devices -"
+TEXT_5 = "Test"
+TEXT_6 = "Sampling rates [Hz]: \n" \
+         "  --\n"\
+         "Bit depths [bit]:\n"\
+         "  --\n"\
+         "Input channels:\n"\
+         "  --\n"\
+         "Output channels:\n"\
+         "  --\n"\
+         "Input latency [ms]:\n"\
+         "  --\n"\
+         "Output latency [ms]:\n"\
+         "  --"
+TEXT_7 = "  2 - Measurement settings  "
+TEXT_8 = "Generated signal"
+TEXT_9 = "MLS length [samples]:"
+TEXT_10 = "Amplitude (0.0 - 1.0):"
+TEXT_11 = "Playback pre-delay [ms]:"
+TEXT_12 = "Recorded signal"
+TEXT_13 = "Expected decay time [s]:"
+TEXT_14 = "Recover default"
+TEXT_15 = "Validate numbers"
+TEXT_16 = "  3 - Output data  "
+TEXT_17 = "Save data to text file:"
+TEXT_18 = "..."
+TEXT_19 = "Plot measured impulse response."
+TEXT_20 = "  4 - Execute measurement  "
+TEXT_21 = "START"
+TEXT_22 = "Status: Idle."
+TEXT_23 = "MSc. Acoustics and Audio Technology, 2015 - Aalborg University"
