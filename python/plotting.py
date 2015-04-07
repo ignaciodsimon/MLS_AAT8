fe655800ed873c825a771d49a5a501207f7c21c5
jdomin14 <jdomin14@student.aau.dk>
2015-04-07 10:41:04 +0200

    Modified to make the plot a bit less ugly.

--- python/plotting.py
+++ python/plotting.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plot
+import strings
 
 
 def plotResults(measurementResult):
     plot.plot(measurementResult.computedImpulseResponse)
+    plot.xlabel(strings.TEXT_33)
+    plot.ylabel(strings.TEXT_34)
+    plot.title(strings.TEXT_35)
+    plot.grid()
     plot.show()
