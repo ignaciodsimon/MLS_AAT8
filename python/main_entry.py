99954d7b50991dd013e5f7af705c1502a0314a1d
jdomin14 <jdomin14@student.aau.dk>
2015-04-07 10:14:37 +0200

    Included "freeze_support()" to make it compatible with windows.

--- python/main_entry.py
+++ python/main_entry.py
@@ -9,28 +9,35 @@ import multiprocessing as mp
 import write_wav
 import plotting
 
+if __name__ == "__main__":
 
-# Runs the interface in another process to "fix" the bug between Tkinter and audio core libraries
-_pool = mp.Pool()
-_result1 = _pool.apply_async(interface.buildInterface)
-_returnedValue = _result1.get()
-_pool.close()
-_pool.join()
+    mp.freeze_support()
 
-# Checks if window was closed or if the "start" button was clicked
-if isinstance(_returnedValue, measurement.MeasurementSettings):
+    # Runs the interface in another process to "fix" the bug between Tkinter and audio core libraries
+    _pool = mp.Pool()
+    _result1 = _pool.apply_async(interface.buildInterface)
+    _returnedValue = _result1.get()
+    _pool.close()
+    _pool.join()
 
-    measurementResult = measurement.executeMeasurement(_returnedValue)
+    # Checks if window was closed or if the "start" button was clicked
+    if isinstance(_returnedValue, measurement.MeasurementSettings):
 
-    if _returnedValue.shouldSaveToFile:
-        print "Saving impulse response to Wav file ..."
-        write_wav.saveImpulseResponseToWav(measurementResult.computedImpulseResponse,
-                                 measurementResult.settings.inputDeviceSamplFreq,
-                                 measurementResult.settings.shouldSaveToFileFilename,
-                                 measurementResult.settings.normalizeOutput)
-        print "Done saving!"
+        print "Executing measurement ..."
+        measurementResult = measurement.executeMeasurement(_returnedValue)
+        print "Measurement done."
 
-    if _returnedValue.shouldPlot:
-        # TODO: make the plot a bit more beautiful ...
-        print "Plotting results ..."
-        plotting.plotResults(measurementResult)
+        if _returnedValue.shouldSaveToFile:
+            print "Saving impulse response to Wav file ..."
+            write_wav.saveImpulseResponseToWav(measurementResult.computedImpulseResponse,
+                                     measurementResult.settings.inputDeviceSamplFreq,
+                                     measurementResult.settings.shouldSaveToFileFilename,
+                                     measurementResult.settings.normalizeOutput)
+            print "Saving done."
+
+        if _returnedValue.shouldPlot:
+            # TODO: make the plot a bit more beautiful ...
+            print "Plotting results ..."
+            plotting.plotResults(measurementResult)
+
+        print "All done."
